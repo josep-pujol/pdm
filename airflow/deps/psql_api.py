a16992651591d15ccc46381a8da2df502b7d734e
josep-pujol <pujol.rabat@gmail.com>
2021-11-03 01:22:52 +0100

    simple client to interact with postgresql

---
+++ airflow/deps/psql_api.py
@@ -0,0 +1,56 @@
+import os
+import urllib
+from sqlalchemy import create_engine
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Psql:
+    """
+    Postgresql Database client
+    """
+
+    def __init__(self):
+        self.host = os.getenv("POSTGRES_HOST")
+        self.port = os.getenv("POSTGRES_PORT")
+        self.usr = os.getenv("POSTGRES_USER")
+        self.pwd = os.getenv("POSTGRES_PASSWORD")
+        self.db_name = os.getenv("POSTGRES_DB")
+        self.conn_obj = None
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} to {self.db_name}>"
+
+    def __enter__(self):
+        print(f"Connecting to {self.db_name}... ", end="")
+
+        def build_conn_string():
+            encoded_pwd = urllib.parse.quote_plus(self.pwd)
+            return (
+                f"postgresql://{self.usr}:{encoded_pwd}"
+                f"@{self.host}:{self.port}/{self.db_name}"
+            )
+
+        def get_conn(connection_string):
+            engine = create_engine(connection_string)
+            self.conn_obj = engine.connect()
+            return self.conn_obj
+
+        conn_string = build_conn_string()
+        return get_conn(conn_string)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.conn_obj.close()
+        print(f"Connection to {self.db_name} closed")
+
+
+def main():
+    with Psql() as conn:
+        res = conn.execute("SELECT version();")
+        print(res.fetchall())
+
+
+if __name__ == '__main__':
+    main()
