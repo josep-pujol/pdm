8f77a0eb988e74ab9ee741941add0b37bfd04a39
josep-pujol <pujol.rabat@gmail.com>
2021-11-08 21:12:39 +0100

    groupped functions and operatior definitions

--- airflow/dags/dag_python_test.py
+++ airflow/dags/dag_python_test.py
@@ -1,13 +1,12 @@
 from datetime import datetime
+import json
+from deps.psql_api import Psql
+from deps.twitter_api import TwitterClient
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from deps.psql_api import Psql
-from deps.twitter_api import TwitterClient
-import json
 
 # These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -17,23 +16,19 @@ default_args = {
 with DAG(
     'dag_python_test',
     default_args=default_args,
-    description='Test Python DAG in Docker container',
+    description='First version of DAG to fetch Tweets and store them in Postgres',
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['test'],
 ) as dag:
 
     def print_context(context):
+        """Just to print information about the following task to execute"""
         print(context)
-        return "context printed"
-
-    t1 = PythonOperator(
-        task_id='print_context',
-        python_callable=print_context,
-        op_kwargs={"context": "FETCHING TWEETS"},
-    )
+        return "Context printed"
 
     def fetch_tweets():
+        """calls the Twitter API to fetch tweets and then store results in postgres-dw"""
         api = TwitterClient()
         tweets = api.get_raw_tweets(query="", count=10000, geo="41.3850639,2.1734035,5km")  # center of Barcelona plus 5km radius  41.3850639, 2.1734035, 5km
         with Psql(db_name="data_lake")  as conn:
@@ -42,6 +37,13 @@ with DAG(
                 tweet_id = t.id
                 tweet_json = json.dumps(t._json)
                 Psql.insert_raw_tweet(conn, tweet_id, tweet_json)
+        return "Tweets fetched and stored in postgres-dw"
+
+    t1 = PythonOperator(
+        task_id='print_context',
+        python_callable=print_context,
+        op_kwargs={"context": "FETCHING TWEETS"},
+    )
 
     t2 = PythonOperator(
         task_id='fetch_tweets',
@@ -49,29 +51,3 @@ with DAG(
     )
 
     t1 >> t2
-
-
-
-    # def short_task(seconds):
-    #     print("Starting short task... ", end="")
-    #     time.sleep(seconds)
-    #     print("short task completed")
-
-    # t2 = PythonOperator(
-    #     task_id='short_task',
-    #     python_callable=short_task,
-    #     op_kwargs={"seconds": 5},
-    # )
-
-    # def long_task(seconds):
-    #     print("Starting long task... ", end="")
-    #     time.sleep(seconds)
-    #     print("long task completed")
-
-    # t3 = PythonOperator(
-    #     task_id='long_task',
-    #     python_callable=long_task,
-    #     op_kwargs={"seconds": 10},
-    # )
-
-    # t1 >> [t2, t3]
