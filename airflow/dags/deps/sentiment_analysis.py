f28deb26c774e59e318e65d93ae9c3306ad64448
josep-pujol <pujol.rabat@gmail.com>
2021-11-18 01:06:34 +0100

    function to generate a sentiment analysis score from text

---
+++ airflow/dags/deps/sentiment_analysis.py
@@ -0,0 +1,20 @@
+from textblob import TextBlob
+
+
+def get_sentiment_score(text):
+    """
+    Utility function to generate sentiment of passed text
+    using textblob's sentiment method
+    """
+    if not isinstance(text, str):
+        print("Text must be of type string: ", text)
+        return
+
+    analysis = TextBlob(text)
+
+    if analysis.sentiment.polarity > 0:
+        return "positive"
+    elif analysis.sentiment.polarity == 0:
+        return "neutral"
+    else:
+        return "negative"
